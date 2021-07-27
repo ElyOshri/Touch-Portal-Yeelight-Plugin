ea366291142f40c181b1614703f40b6323165a27
ElyOshri <79017393+ElyOshri@users.noreply.github.com>
2021-07-27 23:48:29 +0300

    Add files via upload

--- Yeelight_Plugin.py
+++ Yeelight_Plugin.py
@@ -5,9 +5,10 @@ import time
 import sys
 import requests
 import webbrowser
-import os
 from yeelight import discover_bulbs
 from yeelight import Bulb
+from tkinter import *
+
 
 '''
 
@@ -15,21 +16,25 @@ Touch Portal Yeelight plugin
 
 '''
 
-TPHOST = '127.0.0.1'
-TPPORT = 12136
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-
-s.connect((TPHOST, TPPORT))
+s.connect(('127.0.0.1', 12136))
 s.sendall(b'{"type":"pair","id":"TPYeeLight"}\n')
 data = s.recv(1024)
 print(repr(data))
 settings = (json.loads(data.decode('utf-8')))["settings"]
 
+try:
+    permIps = []
+    with open("Permanent_Ips.json") as jsonFile:
+        for i in json.load(jsonFile):
+            permIps.append(i)
+except:
+    permIps = []
+
 
 Running = True
-
 
 
 def DecimalToHex(c):
@@ -41,45 +46,105 @@ def DecimalToHex(c):
     return '#ff%02x%02x%02x' % (int(r), int(g), int(b))
 
 
-def WriteServerData(Serverinfo):
-    '''
-    This Function makes it eazier for write a log without repeating it
-    '''
-    if settings[3]["Enable Log"] == 'On':
-        currenttime = (time.strftime('[%I:%M:%S:%p] '))
+def WriteServerData(ServerInfo):
+    """
+    This Function makes it easier for write a log without repeating it
+    """
+    if settings[4]["Enable Log"] == 'On':
+        currentTime = (time.strftime('[%I:%M:%S:%p] '))
         logfile = open('log.txt', 'a')
-        logfile.write(currenttime + "%s" % (Serverinfo))
+        logfile.write(currentTime + "%s" % ServerInfo)
         logfile.write('\n')
         logfile.close()
-    elif settings[3]["Enable Log"] == 'Off':
-        print(Serverinfo)
+    elif settings[4]["Enable Log"] == 'Off':
+        print(ServerInfo)
 
-if settings[4]["Enable Auto Update"] == "On":
+
+def StartUI():
+    root = Tk()
+    root.title("Yeelight - Permanent Ips")
+    root.geometry("250x350")
+    root.iconphoto(True, PhotoImage(file="icon.png"))
+
+    myListBox = Listbox(root, width=20)
+    myListBox.config(font=("Ariel", 11), justify="center")
+    myListBox.pack(pady=15, padx=15)
+    global permIps
+    for ip in permIps:
+        myListBox.insert(0, ip)
+
+    def delete():
+        global permIps
+        WriteServerData(f"Deleting {myListBox.get(ANCHOR)} From Perm Devices")
+        myListBox.delete(ANCHOR)
+        permIps = []
+        for ip in myListBox.get(0, END):
+            permIps.append(ip)
+        with open("Permanent_Ips.json", "w") as jsonFile:
+            json.dump(permIps, jsonFile)
+
+    def addIp():
+        global permIps
+        WriteServerData(f"Adding {text.get()} To Perm Devices")
+        myListBox.insert(0, text.get())
+        permIps = []
+        for ip in myListBox.get(0, END):
+            permIps.append(ip)
+        with open("Permanent_Ips.json", "w") as jsonFile:
+            json.dump(permIps, jsonFile)
+
+    def helpButton():
+        webbrowser.get().open_new_tab("https://github.com/ElyOshri/Touch-Portal-Yeelight-Plugin#permanent-devices-tutorial")
+
+    Button(root, text="Delete", command=delete).pack(pady=0)
+    Button(root, text="Add New Device", command=addIp).pack(pady=5)
+    Button(root, text="?", command=helpButton, bg="gray", fg="white", font=("Ariel", 10)).pack(pady=5, padx=(5, 225), side=BOTTOM)
+    text = StringVar(root, value="Enter Device Ip Here")
+    Entry(root, textvariable=text, justify="center").pack(pady=5)
+
+    root.mainloop()
+
+
+if settings[5]["Enable Auto Update"] == "On":
+    """
+    This Function is for checking updates on github
+    """
     WriteServerData(f"Checking for updates")
-    CheckingUpdateFile = requests.get("https://api.github.com/repos/ElyOshri/Touch-Portal-yeelight-Plugin/tags").json()
-    if str(CheckingUpdateFile[0]['name']) != "v1.2.1":
-        WriteServerData(f"Found a updated version: {CheckingUpdateFile[0]['name']}")
-        WriteServerData("New version is availble please update")
-        webbrowser.get().open_new_tab('https://github.com/ElyOshri/Touch-Portal-Yeelight-Plugin/releases')
-    if str(CheckingUpdateFile[0]['name']) == "v1.2.1":
-        WriteServerData(f"No new verison is available")
+    try:
+        CheckingUpdateFile = requests.get("https://api.github.com/repos/ElyOshri/Touch-Portal-Yeelight-Plugin/tags", {"User-Agent": "Yeelight Plugin"}).json()
+        if str(CheckingUpdateFile[0]['name']) != "v1.3":  #Todo: Change to new version
+            WriteServerData(f"Found a updated version: {CheckingUpdateFile[0]['name']}")
+            WriteServerData("New version is available please update")
+            webbrowser.get().open_new_tab(f"https://github.com/ElyOshri/Touch-Portal-Yeelight-Plugin/releases/tag/{CheckingUpdateFile[0]['name']}")
+        else:
+            WriteServerData(f"No new version is available")
+    except:
+        WriteServerData("User Passed Update Check Rate Limit")
+
+if settings[3]["Permanent Devices UI"] == "On":
+    threading.Thread(target=StartUI).start()
 
 OLD_DeviceList = []
+oldState = []
+timer = threading.Timer
 
 
-def updatestates():
-    '''
-    This Function for sending data to TouchPortal, Creating new var and removing
-    '''
+def updateStates():
+    """
+    This Function for sending data to TouchPortal and searching for new Devices
+    """
     global OLD_DeviceList
-    timer = threading.Timer(int(settings[0]["State Update Delay"]), updatestates)
+    global oldState
+    global timer
+    timer = threading.Timer(int(settings[0]["State Update Delay"]), updateStates)
     timer.start()
     if Running:
-        DeviceList = []
+        DeviceList = permIps
         DeviceState = []
         try:
             for i in discover_bulbs(int(settings[1]["Discover Devices Delay"])):
-                DeviceList.append(i['ip'])
+                if i["ip"] not in DeviceList:
+                    DeviceList.append(i['ip'])
         except:
             pass
         try:
@@ -89,12 +154,11 @@ def updatestates():
             pass
         for x in DeviceList:
             if x not in OLD_DeviceList:
-                print(f'Found New Device {x} creating a variaible for it')
+                print(f'Found New Device {x} creating a variable for it')
                 s.sendall(('{"type":"createState", "id":"%s", "desc":"YeeLight %s Brightness", "defaultValue":"0"}\n' % ("TPPlugin.Yeelight.device." + x + "CurrentBrightness", x)).encode())
                 s.sendall(('{"type":"createState", "id":"%s", "desc":"YeeLight %s ON or OFF", "defaultValue":"0"}\n' % ("TPPlugin.Yeelight.device." + x + "power", x)).encode())
                 s.sendall(('{"type":"createState", "id":"%s", "desc":"YeeLight %s Hue", "defaultValue":"0"}\n' % ("TPPlugin.Yeelight.device." + x + "hue", x)).encode())
                 s.sendall(('{"type":"createState", "id":"%s", "desc":"YeeLight %s RGB", "defaultValue":"0"}\n' % ("TPPlugin.Yeelight.device." + x + "rgb", x)).encode())
-
 
                 if x not in OLD_DeviceList:
                     OLD_DeviceList.append(x)
@@ -112,58 +176,66 @@ def updatestates():
 
                         OLD_DeviceList.remove(j)
 
-
-        for x in DeviceState:
-            s.sendall(('{"type":"stateUpdate", "id":"%s", "value":"%s"}\n' % ("TPPlugin.Yeelight.device." + list(x)[0] + "CurrentBrightness", int(list(x.values())[0][0]['current_brightness']))).encode())
-            s.sendall(('{"type":"stateUpdate", "id":"%s", "value":"%s"}\n' % ("TPPlugin.Yeelight.device." + list(x)[0] + "power",list(x.values())[0][0]['power'])).encode())
-            if list(x.values())[0][0]['hue'] != None:
-                s.sendall(('{"type":"stateUpdate", "id":"%s", "value":"%s"}\n' % ("TPPlugin.Yeelight.device." + list(x)[0] + "hue", int(list(x.values())[0][0]['hue']))).encode())
-            elif list(x.values())[0][0]['hue'] == None:
-                s.sendall(('{"type":"stateUpdate", "id":"%s", "value":"%s"}\n' % ("TPPlugin.Yeelight.device." + list(x)[0] + "hue", "Hue Is Not Available")).encode())
-            if list(x.values())[0][0]['rgb'] != None:
-                s.sendall(('{"type":"stateUpdate", "id":"%s", "value":"%s"}\n' % ("TPPlugin.Yeelight.device." + list(x)[0] + "rgb", DecimalToHex(int(list(x.values())[0][0]['rgb'])))).encode())
-            elif list(x.values())[0][0]['rgb'] == None:
-                s.sendall(('{"type":"stateUpdate", "id":"%s", "value":"%s"}\n' % ("TPPlugin.Yeelight.device." + list(x)[0] + "rgb", "RGB Is Not Available")).encode())
+        if oldState != DeviceState and DeviceState != []:
+            for x in DeviceState:
+                if x not in oldState:
+                    s.sendall(('{"type":"stateUpdate", "id":"%s", "value":"%s"}\n' % ("TPPlugin.Yeelight.device." + list(x)[0] + "CurrentBrightness", int(list(x.values())[0][0]['current_brightness']))).encode())
+                    s.sendall(('{"type":"stateUpdate", "id":"%s", "value":"%s"}\n' % ("TPPlugin.Yeelight.device." + list(x)[0] + "power", list(x.values())[0][0]['power'])).encode())
+                    if list(x.values())[0][0]['hue'] != None:
+                        s.sendall(('{"type":"stateUpdate", "id":"%s", "value":"%s"}\n' % ("TPPlugin.Yeelight.device." + list(x)[0] + "hue", int(list(x.values())[0][0]['hue']))).encode())
+                    elif list(x.values())[0][0]['hue'] == None:
+                        s.sendall(('{"type":"stateUpdate", "id":"%s", "value":"%s"}\n' % ("TPPlugin.Yeelight.device." + list(x)[0] + "hue", "Hue Is Not Available")).encode())
+                    if list(x.values())[0][0]['rgb'] != None:
+                        s.sendall(('{"type":"stateUpdate", "id":"%s", "value":"%s"}\n' % ("TPPlugin.Yeelight.device." + list(x)[0] + "rgb", DecimalToHex(int(list(x.values())[0][0]['rgb'])))).encode())
+                    elif list(x.values())[0][0]['rgb'] == None:
+                        s.sendall(('{"type":"stateUpdate", "id":"%s", "value":"%s"}\n' % ("TPPlugin.Yeelight.device." + list(x)[0] + "rgb", "RGB Is Not Available")).encode())
+            oldState = DeviceState
     else:
         timer.cancel()
 
 
-updatestates()
-
+updateStates()
 
 while Running:
     '''
     This Keeps this running and wait until it receive a data
     '''
     try:
-        CHUNK_SIZE = 4000
         buffer = bytearray()
-        data = s.recv(CHUNK_SIZE)
-        buffer.extend(data)
+        while True:
+            data = s.recv(1)
+            if data != b'\n':
+                buffer.extend(data)
+            else:
+                break
     except ConnectionResetError:
         WriteServerData(f"Shutdown TPYeeLightPlugin...")
         Running = False
+        timer.cancel()
         sys.exit()
 
-
-    firstline = buffer[:buffer.find(b'\n')]
-    print(firstline)
-    WriteServerData(f"Reviced: {firstline}")
-    d = firstline
+    firstLine = buffer[:buffer.find(b'\n')]
+    print(firstLine)
+    WriteServerData(f"Received: {firstLine}")
+    d = firstLine
     d = json.loads(d)
     if d['type'] == "closePlugin":
         WriteServerData(f"Shutdown TPYeeLightPlugin...")
         Running = False
+        timer.cancel()
         sys.exit()
 
     if d['type'] == 'settings':
         settings[0]['State Update Delay'] = d['values'][0]['State Update Delay']
         settings[1]["Discover Devices Delay"] = d['values'][1]['Discover Devices Delay']
         settings[2]["Enable Disconnected Devices"] = d['values'][2]['Enable Disconnected Devices']
-        settings[3]["Enable Log"] = d['values'][3]['Enable Log']
-        settings[4]["Enable Auto Update"] = d['values'][4]['Enable Auto Update']
+        if settings[3]["Permanent Devices UI"] != d['values'][3]["Permanent Devices UI"] and d['values'][3]["Permanent Devices UI"] == "On":
+            threading.Thread(target=StartUI).start()
+        settings[3]["Permanent Devices UI"] = d['values'][3]["Permanent Devices UI"]
+        settings[4]["Enable Log"] = d['values'][4]['Enable Log']
+        settings[5]["Enable Auto Update"] = d['values'][5]['Enable Auto Update']
 
-    if d['type'] != 'closePlugin' and Running and d['type'] != 'listChange' and d['type'] != "settings" and d['type'] != "broadcast":
+    if Running and d['type'] == "action":
         try:
             if d['data'][0]['value'] != "" and d['data'][1]['value'] != "":
                 try:
@@ -176,7 +248,8 @@ while Running:
                         Bulb(d['data'][1]['value']).turn_off()
 
                     if d['actionId'] == "TPPlugin.YeeLight.Actions.Bright" and d['data'][1]['id'] == "TPPlugin.YeeLight.Actions.DataBright":
-                        WriteServerData(f"Running Action Bright Level {d['data'][0]['value']} On {d['data'][1]['value']}")
+                        WriteServerData(
+                            f"Running Action Bright Level {d['data'][0]['value']} On {d['data'][1]['value']}")
                         Bulb(d['data'][0]['value'], auto_on=True).set_brightness(int(d['data'][1]['value']))
 
                     if d['actionId'] == 'TPPlugin.YeeLight.Actions.Temp' and d['data'][1]['id'] == 'TPPlugin.YeeLight.Actions.DataTemp':
@@ -197,16 +270,15 @@ while Running:
                     if d['actionId'] == 'TPPlugin.YeeLight.Actions.Brightness_Down' and d['data'][1]['id'] == "TPPlugin.YeeLight.Actions.DataBrightDown":
                         WriteServerData(f"Running Action Brightness Down {d['data'][0]['value']} Light")
                         BrightDown = Bulb(d['data'][0]['value']).get_properties(requested_properties=['bright'])
-                        Bulb(d['data'][0]['value'], auto_on=True).set_brightness(int(BrightDown['bright'])-int(d['data'][1]['value']))
+                        Bulb(d['data'][0]['value'], auto_on=True).set_brightness(
+                            int(BrightDown['bright']) - int(d['data'][1]['value']))
 
                     if d['actionId'] == 'TPPlugin.YeeLight.Actions.Brightness_Up' and d['data'][1]['id'] == "TPPlugin.YeeLight.Actions.DataBrightUp":
                         WriteServerData(f"Running Action Brightness Up {d['data'][0]['value']} Light")
                         BrightUp = Bulb(d['data'][0]['value']).get_properties(requested_properties=['bright'])
-                        Bulb(d['data'][0]['value'], auto_on=True).set_brightness(int(BrightUp['bright'])+int(d['data'][1]['value']))
+                        Bulb(d['data'][0]['value'], auto_on=True).set_brightness(
+                            int(BrightUp['bright']) + int(d['data'][1]['value']))
                 except:
                     WriteServerData(f"User Passed Connection Rate Limit")
         except KeyError:
             print('User did not input values')
-
-
-
